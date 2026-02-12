35b32f9a10104a49d8530e7bdca05c8a52dd2bed
anton62k <anton.kashirov@gmail.com>
2026-02-12 21:18:16 +0300

    feat: init

---
+++ src/revisium-client.ts
@@ -0,0 +1,21 @@
+export interface RevisiumClientOptions {
+  baseUrl: string;
+}
+
+export class RevisiumClient {
+  private readonly _baseUrl: string;
+  private _token: string | null = null;
+
+  constructor(options: RevisiumClientOptions) {
+    const url = options.baseUrl;
+    this._baseUrl = url.endsWith('/') ? url.slice(0, -1) : url;
+  }
+
+  public get baseUrl(): string {
+    return this._baseUrl;
+  }
+
+  public isAuthenticated(): boolean {
+    return this._token !== null;
+  }
+}
